e18ed6d6edc3562a70a5832aceb3652e9c1986d0
RuiJ72 <rlagos900@gmail.com>
2022-07-05 21:43:45 -0300

    Ejercicios uno y dos

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Please, enter your name: ");
+string name = Console.ReadLine();
+Console.WriteLine("Your Name is " + name);
+
+
+
+Console.WriteLine("Enter what time it is in your TimeZone: ");
+string time = Console.ReadLine();
+Console.WriteLine("Now it is: " + time + " in your TimeZone");
