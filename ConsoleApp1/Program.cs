312fffa8de79f0d432e700b7295d90734bfe01d5
RuiJ72 <rlagos900@gmail.com>
2022-07-05 22:09:52 -0300

    Mojoras

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,13 @@
 ﻿Console.WriteLine("Please, enter your name: ");
+Console.WriteLine("_________________________");
 string name = Console.ReadLine();
+
 Console.WriteLine("Your Name is " + name);
 
 
-
+Console.WriteLine("########################################");
 Console.WriteLine("Enter what time it is in your TimeZone: ");
 string time = Console.ReadLine();
+Console.WriteLine("########################################");
+
 Console.WriteLine("Now it is: " + time + " in your TimeZone");
